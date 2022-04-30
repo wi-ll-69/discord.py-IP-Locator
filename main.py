a551a5ee1f83903d9ed19a030d1c647598cb63c7
will <103675888+wi-ll-69@users.noreply.github.com>
2022-04-30 14:10:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import discord
 import geocoder
 import folium
 import os
+from html2image import Html2Image
 
 TOKEN = 'token' #copy token here
+
+hti = Html2Image(
+  custom_flags=['--virtual-time-budget=1000']
+)
 
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
@@ -15,27 +20,36 @@ async def on_ready():
 @client.event
 async def on_message(message):
   user_message = str(message.content)
-
-  if message.author == client.user:
-    return
     
-  if '!locate' in user_message:
+  if '$locate' in user_message:
     address = None
     msg_string = user_message.split(' ')
-    msg_string.remove('!locate')
-    if len(msg_string) == 1:
+    msg_string.remove('$locate')
+    if len(msg_string) == 2:
       ip = msg_string[0]
+      zoom_str = msg_string[1]
+      try:
+        zoom = int(zoom_str[5:])
+      except:
+        await message.channel.send('Invalid command')
+        return
+    elif len(msg_string) == 1:
+      ip = msg_string[0]
+      zoom=12
     else:
       await message.channel.send('Invalid command')
       return
     g = geocoder.ip(ip)
     address = g.latlng
     if len(address) > 0:
-      map = folium.Map(location=address, zoom_start=20)
+      map = folium.Map(location=address, zoom_start=zoom)
       folium.Marker(location=address).add_to(map)
       map.save('map.html')
-      await message.channel.send(file=discord.File(r'map.html'))
+      with open('map.html') as f:
+          hti.screenshot(f.read(), save_as='map.jpg')
+      await message.channel.send(file=discord.File(r'map.jpg'))
       os.remove('map.html')
+      os.remove('map.jpg')
       return
     else:
       await message.channel.send('Invalid IP')
@@ -43,4 +57,4 @@ async def on_message(message):
 
 client.run(TOKEN)
 
-#the command is !locate [ip]
+#the command is !locate [ip] [zoom=int]
